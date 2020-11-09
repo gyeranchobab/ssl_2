acf64209e057efe5edc1f4c162d66e23f23fa13a
gyeranchobab <randrandrre@kaist.ac.kr>
2020-11-09 15:34:58 +0900

    add jupyter

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,5 @@
 conda install numpy --yes
 conda install matplotlib==3.1.2 --yes
 conda install scikit-learn --yes
+conda install jupyter --yes
 conda install pytorch torchvision torchaudio cudatoolkit=10.1 -c pytorch --yes
