949947363239866a6802c2302fe91943744bdcea
gyeranchobab <randrandrre@kaist.ac.kr>
2020-11-09 12:57:41 +0900

    add module install script

---
+++ install_dependencies.sh
@@ -0,0 +1,6 @@
+conda create -n proj1 python==3.6
+conda install numpy
+conda install matplotlib==3.1.2
+conda install scikit-learn
+conda install
+conda install pytorch torchvision torchaudio cudatoolkit=10.1 -c pytorch
