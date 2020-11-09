c63ebac554c1ff3e250eec56d6f9f9fc9f439272
gyeranchobab <randrandrre@kaist.ac.kr>
2020-11-09 16:05:50 +0900

    install torch 1.6

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,4 +2,4 @@ conda install numpy --yes
 conda install matplotlib==3.1.2 --yes
 conda install scikit-learn --yes
 conda install jupyter --yes
-conda install pytorch torchvision torchaudio cudatoolkit=10.1 -c pytorch --yes
+conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.1 -c pytorch --yes
