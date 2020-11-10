3a54230bf5f23ffd2c0e8c0ece1ad2d2e95bc2f5
gyeranchobab <randrandrre@kaist.ac.kr>
2020-11-10 09:07:05 +0900

    install file

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,4 +2,5 @@ conda install numpy --yes
 conda install matplotlib==3.1.2 --yes
 conda install scikit-learn --yes
 conda install jupyter --yes
+conda install tqdm --yes
 conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.1 -c pytorch --yes
